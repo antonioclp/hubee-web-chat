1e64d40e805aee44b40adb4fb35fc37357937ef3
antonioclp <antonionetosg6@gmail.com>
2024-10-06 19:16:53 -0300

    first commit

---
+++ apps/frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'Hubee',
+  description: 'Web Chat',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
