93181cbdeb456b790ad037bf7de1a34de3c7fec6
antonioclp <antonionetosg6@gmail.com>
2024-10-07 15:45:28 -0300

    chore: initial project architecture

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
+// Styles.
+import '@/styles/reset.css'
+
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { redirect } from 'next/navigation'
+
 /**
  * Development frontend branch.
  * @version 0.0.1
  * @returns {JSX.Element}
  */
 export default function Home(): JSX.Element {
+  redirect('/login')
   return (
     <main>
       <span>Home</span>
