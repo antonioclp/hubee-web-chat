b19c443b5d5f3b5824c049d484206d894ab833fe
antonioclp <antonionetosg6@gmail.com>
2024-10-07 13:20:59 -0300

    start branch

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+/**
+ * Development frontend branch.
+ * @returns {JSX.Element}
+ */
+export default function Home(): JSX.Element {
   return (
     <main>
       <span>Home</span>
